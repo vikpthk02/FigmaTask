a8442a42ac6fb948ec46e2953ef22f53b006be67
Vivek Pathak <82827056+vikpthk02@users.noreply.github.com>
2022-11-30 20:27:27 +0530

    first commit

---
+++ src/pages/BOOKINGHISTORY.js
@@ -0,0 +1,147 @@
+import styles from "./BOOKINGHISTORY.module.css";
+
+const BOOKINGHISTORY = () => {
+  return (
+    <div className={styles.bOOKINGHISTORYDiv}>
+      <div className={styles.rectangleDiv} />
+      <div className={styles.rectangleDiv1} />
+      <div className={styles.rectangleDiv2} />
+      <img
+        className={styles.dashiconsarrowDownAlt2}
+        alt=""
+        src="../dashiconsarrowdownalt2.svg"
+      />
+      <b className={styles.uPCOMINGAPPOINMENTSB}>UPCOMING APPOINMENTS</b>
+      <b className={styles.newBookingIn2Minutes}>new booking in 2 minutes</b>
+      <div className={styles.september2021930PM}>
+        17 september 2021, 9.30 PM
+      </div>
+      <div className={styles.september2021930PM1}>
+        17 september 2021, 9.30 PM
+      </div>
+      <div className={styles.testXRayTest}>test : X-ray test</div>
+      <div className={styles.doctorWorldClinic}>doctor world clinic</div>
+      <div className={styles.doctorWorldClinic1}>doctor world clinic</div>
+      <div className={styles.appointmentWithDoctorArun}>
+        appointment with doctor arun
+      </div>
+      <div className={styles.appointmentWithDoctorArun1}>
+        appointment with doctor arun
+      </div>
+      <div className={styles.rectangleDiv3} />
+      <img className={styles.rectangleIcon} alt="" src="../rectangle-161.svg" />
+      <img
+        className={styles.rectangleIcon1}
+        alt=""
+        src="../rectangle-183.svg"
+      />
+      <div className={styles.rectangleDiv4} />
+      <div className={styles.rectangleDiv5} />
+      <div className={styles.rectangleDiv6} />
+      <div className={styles.groupDiv}>
+        <img
+          className={styles.rectangleIcon2}
+          alt=""
+          src="../rectangle-1611.svg"
+        />
+        <div className={styles.rectangleDiv7} />
+      </div>
+      <div className={styles.groupDiv1}>
+        <img
+          className={styles.rectangleIcon2}
+          alt=""
+          src="../rectangle-1612.svg"
+        />
+        <div className={styles.rectangleDiv7} />
+      </div>
+      <div className={styles.groupDiv2}>
+        <img
+          className={styles.rectangleIcon4}
+          alt=""
+          src="../rectangle-1613.svg"
+        />
+        <div className={styles.rectangleDiv9} />
+      </div>
+      <div className={styles.groupDiv3}>
+        <img
+          className={styles.rectangleIcon2}
+          alt=""
+          src="../rectangle-1614.svg"
+        />
+        <div className={styles.rectangleDiv7} />
+      </div>
+      <div className={styles.groupDiv4}>
+        <img
+          className={styles.rectangleIcon2}
+          alt=""
+          src="../rectangle-1615.svg"
+        />
+        <div className={styles.rectangleDiv7} />
+      </div>
+      <div className={styles.groupDiv5}>
+        <img
+          className={styles.rectangleIcon2}
+          alt=""
+          src="../rectangle-1616.svg"
+        />
+        <div className={styles.rectangleDiv7} />
+      </div>
+      <div className={styles.groupDiv6}>
+        <img
+          className={styles.rectangleIcon8}
+          alt=""
+          src="../rectangle-1617.svg"
+        />
+        <div className={styles.rectangleDiv13} />
+      </div>
+      <div className={styles.groupDiv7}>
+        <img
+          className={styles.rectangleIcon8}
+          alt=""
+          src="../rectangle-1618.svg"
+        />
+        <div className={styles.rectangleDiv13} />
+      </div>
+      <div className={styles.groupDiv8}>
+        <img
+          className={styles.rectangleIcon10}
+          alt=""
+          src="../rectangle-1619.svg"
+        />
+        <div className={styles.rectangleDiv15} />
+      </div>
+      <div className={styles.groupDiv9}>
+        <img
+          className={styles.rectangleIcon10}
+          alt=""
+          src="../rectangle-16110.svg"
+        />
+        <div className={styles.rectangleDiv15} />
+      </div>
+      <div className={styles.bookNowDiv}>book now</div>
+      <b className={styles.clinicB}>clinic</b>
+      <b className={styles.clinicB1}>clinic</b>
+      <b className={styles.rESCHEDULEB}>rESCHEDULE</b>
+      <b className={styles.rESCHEDULEB1}>rESCHEDULE</b>
+      <b className={styles.cANCELB}>CANCEL</b>
+      <b className={styles.cANCELB1}>CANCEL</b>
+      <b className={styles.bACK}>BACK</b>
+      <div className={styles.testDiv}>Test</div>
+      <b className={styles.testB}>Test</b>
+      <div className={styles.surgeryDiv}>surgery</div>
+      <div className={styles.surgeryDiv1}>surgery</div>
+      <div className={styles.medicineDiv}>medicine</div>
+      <div className={styles.medicineDiv1}>medicine</div>
+      <img className={styles.groupIcon} alt="" src="../group-57.svg" />
+      <img className={styles.groupIcon1} alt="" src="../group-67.svg" />
+      <div className={styles.rectangleDiv17} />
+      <div className={styles.rectangleDiv18} />
+      <b className={styles.vIEWB}>VIEW</b>
+      <b className={styles.vIEWB1}>VIEW</b>
+      <img className={styles.carbonviewIcon} alt="" src="../carbonview.svg" />
+      <img className={styles.carbonviewIcon1} alt="" src="../carbonview.svg" />
+    </div>
+  );
+};
+
+export default BOOKINGHISTORY;
